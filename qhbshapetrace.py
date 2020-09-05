70de2f09bdfb416cb2449f2e58e8c0153ac292f7
Simon Cozens <simon@simon-cozens.org>
2020-09-05 10:16:14 +0100

    Two columns, dim unused lookups

--- qhbshapetrace.py
+++ qhbshapetrace.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QBrush, QColor
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem
 import uharfbuzz as hb
 
@@ -8,6 +8,7 @@ class QHBShapeTrace(QTreeWidget):
         self.vharfbuzz = vharfbuzz
         self.trace = []
         super(QHBShapeTrace, self).__init__()
+        self.setColumnCount(2)
         self.set_text(text)
 
     def set_text(self, text):
@@ -17,20 +18,26 @@ class QHBShapeTrace(QTreeWidget):
         buf.add_str(text)
         buf.guess_segment_properties()
         buf.set_message_func(self.process_msg)
-        self.stack = [ QTreeWidgetItem(["GSUB"]) ]
+        self.stack = [QTreeWidgetItem(["GSUB", ""])]
         self.addTopLevelItem(self.stack[0])
         hb.shape(self.vharfbuzz.hbfont, buf)
 
     def process_msg(self, msg, buf):
-        print(msg)
+        buffernow = self.vharfbuzz.serialize_buf(buf)
+        print(msg, buffernow)
         if msg.startswith("start table GPOS"):
-            self.stack = [ QTreeWidgetItem(["GPOS"]) ]
+            self.stack = [QTreeWidgetItem(["GPOS", ""])]
             self.addTopLevelItem(self.stack[0])
         elif msg.startswith("start lookup "):
             lu = int(msg[13:])
-            c = QTreeWidgetItem([msg[6:]])
+            c = QTreeWidgetItem([msg[6:], buffernow])
             self.stack[-1].addChild(c)
             self.stack.append(c)
         elif msg.startswith("end lookup "):
+            if self.stack[-1].text(1) == buffernow:
+                self.stack[-1].setForeground(0, QBrush(QColor(128,128,128,128)))
+                self.stack[-1].setForeground(1, QBrush(QColor(128,128,128,128)))
+            else:
+                self.stack[-1].setText(1, buffernow)
             self.stack.pop()
         return True
