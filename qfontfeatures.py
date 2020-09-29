d5e8468d1d799d2dc4924d3469d1ef305fb441e1
Simon Cozens <simon@simon-cozens.org>
2020-09-29 17:05:21 +0100

    Start playing with drag-n-drop

--- qfontfeatures.py
+++ qfontfeatures.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QWidget, \
-    QSplitter, QVBoxLayout
+    QSplitter, QVBoxLayout, QAbstractItemView
 from classlist import GlyphClassList
 
 
@@ -20,6 +20,8 @@ class QFontFeaturesPanel(QSplitter):
     def make_free_routine_list(self):
         routine_list = QTreeWidget()
         routine_list.setHeaderLabels(["Routines"])
+        routine_list.setDragEnabled(True)
+        routine_list.setDragDropMode(QAbstractItemView.InternalMove)
         for routine in self.project.fontfeatures.allRoutines():
             name = routine.name or "Anonymous routine"
             routine_item = QTreeWidgetItem([name])
@@ -31,15 +33,21 @@ class QFontFeaturesPanel(QSplitter):
 
     def make_feature_list(self):
         feature_list = QTreeWidget()
+        feature_list.setDragEnabled(True)
+        feature_list.setAcceptDrops(True)
+        # feature_list.setDragDropMode(QAbstractItemView.InternalMove)
         feature_list.setHeaderLabels(["Features"])
         for feature, contents in self.project.fontfeatures.features.items():
             feature_item = QTreeWidgetItem([feature])
             for routine in contents:
                 name = routine.name or "<Routine>"
                 routine_item = QTreeWidgetItem([name])
-                for rule in routine.rules:
-                    rule_item = QTreeWidgetItem([rule.asFea()])
-                    routine_item.addChild(rule_item)
+                routine_item.setFlags(routine_item.flags() & ~Qt.ItemIsDropEnabled)
+                # for rule in routine.rules:
+                #     rule_item = QTreeWidgetItem([rule.asFea()])
+                #     rule_item.setFlags(rule_item.flags() & ~Qt.ItemIsDropEnabled)
+                #     rule_item.setFlags(rule_item.flags() & ~Qt.ItemIsDragEnabled)
+                #     routine_item.addChild(rule_item)
                 feature_item.addChild(routine_item)
             feature_list.addTopLevelItem(feature_item)
         return feature_list
