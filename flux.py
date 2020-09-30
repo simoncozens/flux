9c012851543d5c7117284770ae4173685e8d27ea
Simon Cozens <simon@simon-cozens.org>
2020-09-30 11:28:57 +0100

    Make editor class

--- flux.py
+++ flux.py
@@ -1,6 +1,14 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QApplication, QHBoxLayout, QLineEdit, QSplitter
+from PyQt5.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QApplication,
+    QHBoxLayout,
+    QLineEdit,
+    QSplitter,
+    QStackedWidget,
+)
 from fontFeatures.ttLib import unparse
 from fontTools.ttLib import TTFont
 from fontFeatures.optimizer import Optimizer
@@ -8,6 +16,7 @@ from qfontfeatures import QFontFeaturesPanel
 from qbufferrenderer import QBufferRenderer
 from qhbshapetrace import QHBShapeTrace
 from qshapingdebugger import QShapingDebugger
+from qruleeditor import QRuleEditor
 from fontFeatures.feeLib import FeeParser
 from ttfontinfo import TTFontInfo
 from fluxproject import FluxProject
@@ -17,24 +26,34 @@ import sys
 app = QApplication(sys.argv)
 
 proj = FluxProject(sys.argv[1])
-proj.fontfeatures.features["mark"] = [proj.fontfeatures.routines[2]]
-proj.fontfeatures.features["curs"] = [proj.fontfeatures.routines[1]]
-
-w = QWidget()
-w.resize(510, 210)
-
-v_box_1 = QVBoxLayout()
-v_box_1.addWidget(QFontFeaturesPanel(proj))
-
-v_box_2 = QVBoxLayout()
-v_box_2.addWidget(QShapingDebugger(proj))
-
-h_box = QHBoxLayout()
-h_box.addLayout(v_box_1)
-h_box.addLayout(v_box_2)
-
-w.setLayout(h_box)
 
 
-w.show()
+class FluxEditor(QWidget):
+    def __init__(self, proj):
+        super(QWidget, self).__init__()
+        self.project = proj
+        v_box_1 = QVBoxLayout()
+        v_box_1.addWidget(QFontFeaturesPanel(proj, self))
+
+        v_box_2 = QVBoxLayout()
+        self.stack = QStackedWidget()
+        self.shapingDebugger = QShapingDebugger(proj)
+        self.ruleEditor = QRuleEditor(proj, None)
+        self.stack.addWidget(self.shapingDebugger)
+        self.stack.addWidget(self.ruleEditor)
+        v_box_2.addWidget(self.stack)
+
+        h_box = QHBoxLayout()
+        h_box.addLayout(v_box_1)
+        h_box.addLayout(v_box_2)
+
+        self.setLayout(h_box)
+
+    def showRuleEditor(self, rule):
+        self.ruleEditor.setRule(rule)
+        self.stack.setCurrentIndex(1)
+        pass
+
+
+FluxEditor(proj).show()
 sys.exit(app.exec_())
